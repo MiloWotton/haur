7a04d83bccd013991be46949118630c9109d6e1f
MiloWotton <42776@farnborough.ac.uk>
2024-11-07 18:35:57 +0000

    Add files via upload

---
+++ 2 week project/2 week project/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a word:");
+        String word = input.nextLine();
+
+        System.out.println("What would you like to shift by?:");
+        int lineShift = input.nextLine();
+
+        
+
+    }
+}
